6c451a2b5bf067952b51fe78ea8ca33c0e6916c4
joseph-GM <jkim.english75@gmail.com>
2021-09-03 07:43:04 +0900

    first commit

---
+++ app/src/main/java/org/techtown/recycleviewexample/MainActivity.java
@@ -0,0 +1,18 @@
+package org.techtown.recycleviewexample;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    private RecyclerView contactsRecView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        contactsRecView = findViewById(R.id.contactsRecView);
+    }
+}
