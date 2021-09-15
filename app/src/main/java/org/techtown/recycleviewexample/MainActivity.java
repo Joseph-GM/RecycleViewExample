3c0b49db1c801c4fba4675701757ba025387797d
Joseph-GM <jkim.english75@gmail.com>
2021-09-15 14:20:51 +0900

    final finished part3

--- app/src/main/java/org/techtown/recycleviewexample/MainActivity.java
+++ app/src/main/java/org/techtown/recycleviewexample/MainActivity.java
@@ -24,15 +24,15 @@ public class MainActivity extends AppCompatActivity {
         ArrayList<Contact> contacts = new ArrayList<>();
         contacts.add(new Contact("Joseph Kim", "joseph.kim@gm.com","https://butterpopcorn.kr/wp-content/uploads/2020/08/%EC%A0%9C%EB%AA%A9-%EC%97%86%EC%9D%8C-1-16.jpg"));
         contacts.add(new Contact("Changsun Lee", "changsun.lee@gm.com","https://upload.wikimedia.org/wikipedia/commons/c/ca/Denzel_Washington_cropped.jpg"));
-        contacts.add(new Contact("Juhyung Sun", "juhyung.sun@gm.com","http://www.futurekorea.co.kr/news/photo/201506/27911_18694_123.JPG"));
+        contacts.add(new Contact("Juhyung Sun", "juhyung.sun@gm.com","https://phantom-marca.unidadeditorial.es/198490d8749c76d6bce8ebb4ac39b101/resize/1320/f/jpg/assets/multimedia/imagenes/2021/08/29/16302359860434.jpg"));
         contacts.add(new Contact("Changhwan Lee", "changhwan.lee@gm.com","https://file.mk.co.kr/meet/neds/2021/07/image_readtop_2021_704239_16269130774725174.jpg"));
-        contacts.add(new Contact("Byungjong Yu", "byungjong.yu@gm.com","http://pds.joins.com/news/component/htmlphoto_mmdata/201604/21/htm_20160421164314998976.jpg"));
+        contacts.add(new Contact("Byungjong Yu", "byungjong.yu@gm.com","https://newsimg.hankookilbo.com/cms/articlerelease/2019/10/21/201910210959082880_1.jpg"));
 
         ContactsRecViewAdapter adapter = new ContactsRecViewAdapter(this);
         adapter.setContacts(contacts);
 
         contactsRecView.setAdapter(adapter);
-        contactsRecView.setLayoutManager(new GridLayoutManager(this, 2));
+        contactsRecView.setLayoutManager(new GridLayoutManager(this,2));
 
     }
 }
